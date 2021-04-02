d7d51a1fc3f79c0401bbe7268adcee71805236f0
kiranpranay <kiranpranay12@gmail.com>
2021-04-02 23:52:53 +0530

    f

--- test.php
+++ test.php
@@ -1,2 +1,13 @@
 <?php 
-echo "hello";
+session_start();
+$con = mysqli_connect("34.93.127.54","root","123456","test");
+if(mysqli_connect_errno()) {  
+    die("Failed to connect with MySQL: ". mysqli_connect_error());  
+}
+
+$res=mysqli_query($con,"SELECT * FROM `test`");
+while($row=mysqli_fetch_assoc($res)){
+    print_r($row);
+}
+
+?>
