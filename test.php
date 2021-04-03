3422334bb1d4e0783de209654b5a14ed63011f67
kiranpranay <kiranpranay12@gmail.com>
2021-04-03 17:02:21 +0530

    db

--- test.php
+++ test.php
@@ -1,6 +1,22 @@
 <?php 
 session_start();
-$con = mysqli_connect("34.93.127.54","root","123456","test");
+$con = mysqli_connect("localhost","root","mysql","stm");
+if(mysqli_connect_errno()) {  
+    die("Failed to connect with MySQL: ". mysqli_connect_error());  
+}
+
+$res=mysqli_query($con,"SELECT * FROM `users`");
+while($row=mysqli_fetch_assoc($res)){
+    print_r($row);
+    echo "im in loop";
+}
+echo "out";
+
+?>
+
+<!-- <?php 
+session_start();
+$con = mysqli_connect("localhost","root","","test");
 if(mysqli_connect_errno()) {  
     die("Failed to connect with MySQL: ". mysqli_connect_error());  
 }
@@ -12,4 +28,4 @@ while($row=mysqli_fetch_assoc($res)){
 }
 echo "out";
 
-?>
+?> -->
