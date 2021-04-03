fb87d4192573dae9ec3ffbb0f82347d984712742
root <root@cotton.us-central1-a.c.divine-vehicle-303815.internal>
2021-04-03 13:24:11 +0000

    ii

--- test2.php
+++ test2.php
@@ -1,10 +1,21 @@
-<?php 
-include "DbConnect.php"; 
-$db = new DbConnect();
-                $conn = $db->connect();
-                $sql = "SELECT * FROM test";
-                while($row=mysqli_fetch_assoc($sql)){
-                    echo $row['name'];
-                }
-                
-echo "out";
+<?php
+echo "php start";
+
+$con = mysqli_connect( "34.93.127.54" , "root" , "123456" , "test");
+
+// Check connection
+if (mysqli_connect_errno())
+  {
+  echo "Failed to connect to MySQL: " . mysqli_connect_error();
+  }
+
+mysqli_query( $con , "INSERT INTO `test` (`id`, `name`) VALUES (NULL, 'NUUUU');");
+$res=mysqli_query( $con , "SELECT * FROM `test`");
+while($row=mysqli_fetch_assoc($res)){
+echo $row['name'];
+}
+mysqli_close($con);
+
+echo "php end";
+?>
+
